4e75c97ac4a3c075863b57162e5af6072ac40575
jimmy-oss <jimmynjonge51@gmail.com>
2023-03-18 13:05:04 +0300

    comments recap

---
+++ example.py
@@ -0,0 +1,15 @@
+import math
+
+x = math.sqrt(9)
+print(x)
+
+y = math.ceil(3)
+print(y)
+
+ # pylint: disable=W0105
+"""
+The math module comes with a lot of built-in methods,
+the sqrt() method to finds the square root of 9 (3.0), 
+which we represent with the variable x; and the ceil() method to round up x (3).
+"""
+ 
